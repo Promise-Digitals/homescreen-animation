57388134fec54d03c4000ea0a572f796fc8643c6
Promise Digitals <144558838+Promise-Digitals@users.noreply.github.com>
2023-11-04 12:27:24 -0700

    updated myscript.js

--- myscript.js
+++ myscript.js
@@ -1,2 +1,16 @@
 let content = document.querySelectorAll('.content');
-let index = 
+let index = 0;
+
+// next function
+function next(){
+    content[index].classList.remove('show');
+    index = (index + 1) % content.length;
+    content[index].classList.add('show');
+}
+
+// prev function
+function prev(){
+    content[index].classList.remove('show');
+    index = (index - 1 + content.length) % content.length;
+    content[index].classList.add('show');
+}
